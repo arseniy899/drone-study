9e142bb83195c7c64ad57083013d311742dad76a
Arseniy899 <arseniy.p@mail.ru>
2021-12-03 15:34:38 +0300

    Initial commit

---
+++ Assets/CheckpointTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointTrigger : MonoBehaviour
+{
+    public int missionItemId = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (missionItemId != 0)
+        {
+            gameObject.SetActive(false);
+        }
+        MissionManager.assignCheckpoint(missionItemId, gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        bool isConfirmed = MissionManager.reportCheckpointEnter(missionItemId);
+        if (isConfirmed)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/DroneChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneChange : MonoBehaviour
+{
+    public List<GameObject> drones;
+    int curDrone = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void SwitchDrone()
+    {
+        if( (curDrone+1) == drones.Count )
+        {
+            curDrone = 0;
+        }
+        else
+        {
+            curDrone++;
+        }
+        for(int i = 0; i < drones.Count; i++)
+        {
+            drones[i].SetActive(i == curDrone);
+        }
+    }
+}
